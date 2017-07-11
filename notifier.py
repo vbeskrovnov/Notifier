c3ec047e7080688ada7f43a0d8d2e40ebedbc361
vabe0516 <vadim.beskrovnov@netcracker.com>
2017-07-11 19:16:09 +0400

    Add notify logic

--- notifier.py
+++ notifier.py
@@ -1,2 +1,12 @@
+from skpy import Skype
+
+chat_bot_login = ""
+chat_bot_password = ""
+chat_url = ""
+
+skype = Skype(chat_bot_login, chat_bot_password)
+
+
 def notify(message):
-    print message
+    chat = skype.chats.chat(skype.chats.urlToIds(chat_url)['id'])
+    chat.sendMsg(message)

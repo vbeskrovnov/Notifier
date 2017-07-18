8730c187ffa8c97b353b98023326e6a9554d5c7b
Vadim Beskrovnov <seede1@yandex.ru>
2017-07-18 21:47:07 +0400

    Clean code

--- notifier.py
+++ notifier.py
@@ -8,19 +8,9 @@ def get_plugins(json_input):
     return json_input["plugins"]
 
 
-def load_plugin(name):
-    mod = __import__(name)
-    return mod
-
-
-def call_plugin(name, *args, **kwargs):
-    plugin = load_plugin(name)
-    plugin.plugin_main(*args, **kwargs)
-
-
 def read_config():
-    f = open('config.json')
-    return f
+    config_file = open('config.json')
+    return config_file
 
 
 def run():
@@ -33,11 +23,11 @@ def run():
             output_path = plugin["output"]["script"]
             output_name = plugin["output"]["name"]
 
-            input_plugin = imp.load_source(input_name, input_path)
-            output_plugin = imp.load_source(output_name, output_path)
+            input_script = imp.load_source(input_name, input_path)
+            output_script = imp.load_source(output_name, output_path)
 
-            for message in input_plugin.input():
-                output_plugin.output(message)
+            for message in input_script.input():
+                output_script.output(message)
 
         time.sleep(5)
 

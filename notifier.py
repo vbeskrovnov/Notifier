c4c65760b30938853ade3ebb031bfeffb06f338d
vabe0516 <vadim.beskrovnov@netcracker.com>
2017-07-18 12:00:38 +0400

    Remove time interval from config and add test realisation for notifier

---
+++ notifier.py
@@ -0,0 +1,15 @@
+
+def get_inputs(json):
+    return []
+
+def run():
+    json = ""
+    while True:
+        for input_plugin in get_inputs(json):
+            path = input_plugin["script"]
+
+
+    print 123
+
+
+run()
